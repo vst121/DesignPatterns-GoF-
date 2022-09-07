fbf6aaea3ce91622c9706b4e433402986b752c16
vahid <vst121@gmail.com>
2022-09-07 19:35:26 +0430

    Add Singleton Pattern

---
+++ Sibgleton/ConfigManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class ConfigManager
+    {
+        private static ConfigManager instance = new ConfigManager();
+        private Dictionary<string, object> settings = new Dictionary<string, object>();
+
+        public static ConfigManager Instance { get => instance; }
+
+        private ConfigManager()
+        {
+        }
+
+        public void Set(string key, object value)
+        {
+            settings.Add(key, value);
+        }
+        public object Get(string key)
+        {
+            return settings[key];
+        }
+    }
+}
---
+++ Sibgleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "CourseName";
+            
+            var configManager1 = ConfigManager.Instance;
+            configManager1.Set(key, "Design Patterns");
+            Console.WriteLine(configManager1.Get(key));
+
+            var configManager2 = ConfigManager.Instance;
+            Console.WriteLine(configManager2.Get(key));
+
+            Console.ReadLine();
+        }
+    }
+}
